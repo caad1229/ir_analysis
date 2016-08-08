a414fdb6ffae93136aad862dfb2a681e67f1f493
s.s <caad1229@gmail.com>
2016-08-08 23:33:49 +0900

    receive ir

---
+++ src/main.ino
@@ -0,0 +1,19 @@
+#include <IRremote.h>
+
+#define RECV_PIN 11
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup() {
+    Serial.begin(9600);
+    irrecv.enableIRIn();
+}
+void loop() {
+    if (irrecv.decode(&results)) {
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+    }
+    delay(100);
+}
